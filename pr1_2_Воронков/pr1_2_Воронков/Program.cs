90fca07815d148d7bd09d302c1b798c1d17431f7
v3698741 <80327378+v3698741@users.noreply.github.com>
2021-04-01 12:12:41 +0300

    Add files via upload

---
+++ pr1_2_Воронков/pr1_2_Воронков/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pr1_2_Воронков
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение c: ");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение d: ");
+            double d = double.Parse(Console.ReadLine());
+
+            double func = Math.Pow((a + b / c + d), 2) + Math.Pow(b,2);
+
+            Console.WriteLine("Значение выражения: " + func);
+            Console.ReadLine();
+        }
+    }
+}
